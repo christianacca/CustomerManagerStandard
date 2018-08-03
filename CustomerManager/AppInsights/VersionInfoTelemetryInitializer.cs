e181fcd05b9daac20b32ed287e35c1aafd18d7b7
christianacca <christian.crowhurst@gmail.com>
2018-08-03 11:54:29 +0100

    add application version to telemetry

---
+++ CustomerManager/AppInsights/VersionInfoTelemetryInitializer.cs
@@ -0,0 +1,15 @@
+﻿using CustomerManager.App_Start;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace CustomerManager.AppInsights
+{
+    public class VersionInfoTelemetryInitializer: ITelemetryInitializer
+    {
+        public void Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Component.Version =
+                typeof(WebApiConfig).Assembly.GetName().Version.ToString();
+        }
+    }
+}
